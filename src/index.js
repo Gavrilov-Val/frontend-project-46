cd615d6071ff5da06eb4fe5c1777dba6ca2b5e68
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-06-12 15:23:25 -0600

    install _, implement simple json genDiff

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const getFilepath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getParsedData = (data) => JSON.parse(data);
 
-const showFilesData = (filepath1, filepath2) => {
+const showDiff = (filepath1, filepath2) => {
   const absPath1 = getFilepath(filepath1);
   const absPath2 = getFilepath(filepath2);
   const data1 = fs.readFileSync(absPath1, 'utf-8');
   const data2 = fs.readFileSync(absPath2, 'utf-8');
   const parsedData1 = getParsedData(data1);
   const parsedData2 = getParsedData(data2);
-  return {
-    filepath1: absPath1,
-    filepath2: absPath2,
-    data1: parsedData1,
-    data2: parsedData2,
-  };
+  const sortedKeys = _.sortBy(_.union(_.keys(parsedData1), _.keys(parsedData2)));
+  const diffLines = sortedKeys.map((key) => {
+    const hasInData1 = _.has(parsedData1, key);
+    const hasInData2 = _.has(parsedData2, key);
+    
+    if (!hasInData1) {
+      return `  + ${key}: ${parsedData2[key]}`;
+    }
+    
+    if (!hasInData2) {
+      return `  - ${key}: ${parsedData1[key]}`;
+    }
+    
+    if (parsedData1[key] === parsedData2[key]) {
+      return `    ${key}: ${parsedData1[key]}`;
+    }
+    
+    return [
+      `  - ${key}: ${parsedData1[key]}`,
+      `  + ${key}: ${parsedData2[key]}`
+    ].join('\n');
+  });
+
+  return `{\n${diffLines.join('\n')}\n}`;  
 };
 
-//console.log(showFilesData('../files/file1.json', '../files/file2.json'));
 
-export default showFilesData;
+
+export default showDiff;
