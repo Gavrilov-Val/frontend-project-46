bdcbd28d59ff2ccd2a2625a1b9eddf9353d97c8d
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-06-26 09:35:31 -0600

    add diff for nested structures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,7 @@ import showDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), { encoding: 'utf8' });
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const expectedFileJson = readFile('expectedFileJson.txt');
 
 test('showDiff with JSON files', () => {
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,11 +9,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2/* , options */) => {
-    // Здесь будет логика сравнения файлов
-    console.log(showDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(showDiff(filepath1, filepath2, options.format));
   });
 
 program.parse(process.argv);
